f809d3dcec665dae661b205bda52bcd05b4200d4
Hendrik <hendrik.vloet@psiori.com>
2022-06-23 18:05:59 +0200

    Fix tmux dotfile install script.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 function install_tmux_dotfiles(){
-	cp tmux/tmux.conf $HOME/
+	cwd=$(pwd)
+	echo $cwd
+	cp $cwd/tmux/tmux.conf $HOME/
+	mv $HOME/tmux.conf $HOME/.tmux.conf
 	mkdir -p $HOME/.tmux
-	cp tmux/left_status.sh $HOME/.tmux
+	cp $cwd/tmux/bash-extensions/left_status.sh $HOME/.tmux
 	chmod +x $HOME/.tmux/left_status.sh
 }
+
+install_tmux_dotfiles
